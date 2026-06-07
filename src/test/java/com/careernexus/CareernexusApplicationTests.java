3835dd01dae0abe27815443c5df6af4a82061fc3
Janhavi <malijanvi54@gmail.com>
2026-06-07 22:34:44 +0530

    Spring Boot project setup completed

---
+++ src/test/java/com/careernexus/CareernexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careernexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareernexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
